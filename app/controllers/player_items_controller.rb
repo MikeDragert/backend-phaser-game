2d67f831a086faa420136cdc0701dd8a53ed87c4
GardRyan <res.gardner@gmail.com>
2024-03-23 12:53:30 -0400

    added create params to player_items

--- app/controllers/player_items_controller.rb
+++ app/controllers/player_items_controller.rb
@@ -2,4 +2,11 @@ class PlayerItemsController < ApplicationController
   def index
     render json: PlayerItem.player_item_item.all
   end
+
+  def create
+    player = Player.find(params[:player_id])
+    item = Item.find(params[:item_id])
+    player_item = PlayerItem.create(player: player, item: item)
+    render json: player_item
+  end
 end
