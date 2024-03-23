fdb305f358f88be8b93ad4bb18a42b528811ea07
GardRyan <res.gardner@gmail.com>
2024-03-23 13:39:02 -0400

    updated player routes to include :update

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   # root "posts#index"
   resources :items, only: [:index]
   resources :saves, only: [:index]
-  resources :players, only: [:index]
+  resources :players, only: [:index, :update]
   resources :achievements, only: [:index]
   resources :highscores, only: [:index]
   resources :player_achievements, only: [:index]
   resources :player_items, only: [:index]
-    
+  
+  # Players Post Routes
   post '/players/:player_id/saves', to: 'saves#create'
   post '/players/:player_id/player_items', to: 'player_items#create'
   post '/players/:player_id/player_achievements', to: 'player_achievements#create'
@@ -21,5 +22,8 @@ Rails.application.routes.draw do
   post '/players/:player_id/items', to: 'items#create'
   post '/players/:player_id/saves', to: 'saves#create'
   post '/players/:player_id/player_items', to: 'player_items#create'
+
+  # Login and Register Routes
+  post '/login', to: 'sessions#create'
  
 end
