e4f09d3645c9a2c5750cbc620dcf4ed38f4a4e80
GardRyan <res.gardner@gmail.com>
2024-03-23 12:30:00 -0400

    added intial post routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,14 @@ Rails.application.routes.draw do
   resources :player_achievements, only: [:index]
   resources :player_items, only: [:index]
     
+  post '/players/:player_id/saves', to: 'saves#create'
+  post '/players/:player_id/player_items', to: 'player_items#create'
+  post '/players/:player_id/player_achievements', to: 'player_achievements#create'
+  post '/players/:player_id/highscores', to: 'highscores#create'
+  post '/players', to: 'players#create'
+  post '/players/:player_id/achievements', to: 'achievements#create'
+  post '/players/:player_id/items', to: 'items#create'
+  post '/players/:player_id/saves', to: 'saves#create'
+  post '/players/:player_id/player_items', to: 'player_items#create'
+ 
 end
