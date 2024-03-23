a4997a599f90ab330759fa0719e189105f5ca27c
Mike Dragert <mikedragert@gmail.com>
2024-03-23 19:50:06 -0400

    Added more player_item seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
-# Clear existing items
+# Clear existing data - in proper order
+PlayerItem.destroy_all
+PlayerSave.destroy_all
+Player.destroy_all
 Item.destroy_all
 
 # Seed items
 items_data = [
   { name: "Larry", item_type: 10 },
-  { name: "Jump Function", item_type: 3 },
-  { name: "Size Function", item_type: 3 },
-  { name: "Loop Function", item_type: 3 },
+  { name: "Function_jumpPower", item_type: 3 },
+  { name: "Function_playerSize", item_type: 3 },
+  { name: "Function_passKey", item_type: 3 },
   { name: "Something Special Function", item_type: 3  },
   { name: "Number_1", item_type: 1 },
   { name: "Number_2", item_type: 1 },
@@ -22,6 +25,7 @@ items_data = [
   { name: "Operator_-", item_type: 2 },
   { name: "Operator_*", item_type: 2 },
   { name: "Operator_/", item_type: 2 },
+  { name: "Operator_%", item_type: 2 },
   { name: "coin", item_type: 11 },
 ]
 
@@ -30,7 +34,7 @@ items_data.each do |item_data|
 end
 
 # Clear existing players
-Player.destroy_all
+
 
 # Seed players
 players_data = [
@@ -40,6 +44,7 @@ players_data = [
 players_data.each do |player_data|
   Player.create!(player_data)
 end
+
 
 # Seed saves
 player = Player.find_by(username: "abcd")
@@ -51,18 +56,258 @@ saves_data.each do |save_data|
   PlayerSave.create!(save_data)
 end
 
+
 # Seed player items
-item = Item.find_by(name: "Larry")
+itemLarry = Item.find_by(name: "Larry")
+
+itemJump = Item.find_by(name: "Function_jumpPower")
+itemSize = Item.find_by(name: "Function_playerSize")
+itemPassKey = Item.find_by(name: "Function_passKey")
+itemN1 = Item.find_by(name: "Number_1")
+itemN2 = Item.find_by(name: "Number_2")
+itemN3 = Item.find_by(name: "Number_3")
+itemN4 = Item.find_by(name: "Number_4")
+itemN5 = Item.find_by(name: "Number_5")
+itemN6 = Item.find_by(name: "Number_6")
+itemN7 = Item.find_by(name: "Number_7")
+itemN8 = Item.find_by(name: "Number_8")
+itemN9 = Item.find_by(name: "Number_9")
+itemN10 = Item.find_by(name: "Number_10")
+itemOPlus = Item.find_by(name: "Operator_+")
+itemOMinus = Item.find_by(name: "Operator_-")
+itemOMult = Item.find_by(name: "Operator_*")
+itemODiv = Item.find_by(name: "Operator_/")
+itemOMod = Item.find_by(name: "Operator_%")
+itemCoin = Item.find_by(name: "coin")
+
+
 save = PlayerSave.find_by(player_id: player.id)
 
-if player && item && save
+if player && save
   PlayerItem.create!(
     player_id: player.id,
-    item_id: item.id,
+    item_id: itemLarry.id,
     player_save_id: save.id,
     container_item_id: 1,
     location_x: 1,
     location_y: 1,
     map_id: 'STARTITEM'
   )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemLarry.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemJump.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemSize.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemPassKey.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN1.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN2.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN3.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN4.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN5.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN6.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN7.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN8.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN9.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemN10.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemOPlus.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemOMinus.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemODiv.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemOMult.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemOMod.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemCoin.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemCoin.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemCoin.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemCoin.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+  PlayerItem.create!(
+    player_id: player.id,
+    item_id: itemCoin.id,
+    player_save_id: save.id,
+    container_item_id: 1,
+    location_x: 1,
+    location_y: 1,
+    map_id: 'STARTITEM'
+  )
+
 end
