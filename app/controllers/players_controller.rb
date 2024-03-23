81f1e4cad03d5ce8c3657066e21d86aa584b0671
GardRyan <res.gardner@gmail.com>
2024-03-23 13:38:41 -0400

    updated params for validation of users.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,5 +4,27 @@ class PlayersController < ApplicationController
   end
 
   def create
-    player = Player.new(player_params)  
+    @player = Player.new(player_params)
+
+    if @player.save
+      render json: @player, status: :created
+    else
+      render json: @player.errors, status: :unprocessable_entity
+    end
+  end
+
+    def update
+    @player = Player.find(params[:id])
+    if @player.update(player_params)
+      render json: @player, status: :ok
+    else
+      render json: @player.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:username, :email, :password)
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
-  # has_many :saves
-  has_many :player_achievements
+  has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true,
 end
