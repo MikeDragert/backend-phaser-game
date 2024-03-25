d80657a2c3b9f8151345cf11f98a6f50aad387f3
GardRyan <res.gardner@gmail.com>
2024-03-25 16:54:55 -0400

    updated model for scores

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,16 @@
 class Player < ApplicationRecord
   has_secure_password
   has_many :player_saves
+  has_many :player_items
+  has_many :player_achievements
+  has_many :items, through: :player_items
   
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
+
+
+  def calculate_score
+    items.sum(&:score)
+  end
 end
