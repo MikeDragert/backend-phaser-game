617a81029c89cb49f870bde4aa29553243095439
GardRyan <res.gardner@gmail.com>
2024-03-25 17:21:35 -0400

    updated migration for achievements

--- db/migrate/20240321204337_create_achievements.rb
+++ db/migrate/20240321204337_create_achievements.rb
@@ -2,7 +2,7 @@ class CreateAchievements < ActiveRecord::Migration[7.1]
   def change
     create_table :achievements, id: :serial do |t|
       t.string "title"
-
+      t.string "description"
     end
   end
 end
