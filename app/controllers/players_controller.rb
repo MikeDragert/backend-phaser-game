42aaf9a197231a320547095162ee09254995715b
GardRyan <res.gardner@gmail.com>
2024-03-25 20:59:52 -0400

    api wasn't sending scores data for highscores. updated players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -13,29 +13,28 @@ class PlayersController < ApplicationController
     end
   end
 
-    def update
+  def update
     @player = Player.find(params[:id])
     if @player.update(player_params)
       render json: @player, status: :ok
     else
       render json: @player.errors, status: :unprocessable_entity
     end
+  end
 
-    def calculate_scores
-      scores = Player.all.map do |player|
-        { player_id: player.id, score: player.items.sum(:score) }
-      end
-    
-      sorted_scores = scores.sort_by { |score| score[:score] }.reverse
-    
-      render json: sorted_scores
+  def calculate_scores
+    scores = Player.all.map do |player|
+      { player_id: player.id, score: player.items.sum(:score) }
     end
+
+    sorted_scores = scores.sort_by { |score| score[:score] }.reverse
+
+    render json: sorted_scores
+  end
 
   private
 
   def player_params
     params.require(:player).permit(:username, :email, :password)
   end
-
-  
 end
